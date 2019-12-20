fe14d0084b0faa1ba148866a3fe103d3a414e19a
TL <tom.kennedy.lee@gmail.com>
2019-12-20 03:40:35 -0500

    minot updates

--- dash.py
+++ dash.py
@@ -2,9 +2,10 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-app = dash.Dash()
+app = dash.dash()
 app.layout = html.Div('Dash Tutorial')
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
     
